fd1cb235c0d51ad0e9e69c5b7856e8f63fcfa2a0
shashank <shashank@lightinfosys.com>
2020-08-26 20:23:58 +0530

    added kaprekar routine

--- kaprekar_constant/__init__.py
+++ kaprekar_constant/__init__.py
@@ -1,4 +1,4 @@
 __version__ = "0.0.1"
 __author__ = "Shashank Deshpande"
-from kaprekar_constant.main import *
+from kaprekar_constant.main import get_steps, step_count, kaprekar_routine
 
--- kaprekar_constant/main.py
+++ kaprekar_constant/main.py
@@ -1,21 +1,52 @@
-def get_steps(num):
-    num = int(num)
-    steps = []
-    prev_num = 0
-    if num not in range(1,10000):
+def kaprekar_routine(n):
+    """
+        Input  : 4 digit Integer Number
+                 1, 2, 3 digit numbers are also valid, we will pad those with leading zeros.
+                 Allowed range - [1-9999]
+
+        Output : Kaprekar routine steps to reach constant 0/6174.
+    """
+    n = int(n)
+    check_range(n)
+    routine = []
+    prev_n = 0
+    while n != prev_n:
+        prev_n = n
+        n_str = pad_leading_zeros(n)
+        asc = int("".join(sorted(n_str)))
+        desc = int("".join(sorted(n_str, reverse=True)))
+        n = desc - asc
+        if n != prev_n:
+            routine.append(f"{desc} - {asc} = {n}")
+    return len(routine), routine
+
+def pad_leading_zeros(n):
+    """
+        This function will pad 1,2,3 digit numbers with leading zeros and convert to string.
+    """
+    n_str = str(n).zfill(4)
+    return n_str
+
+def check_range(n):
+    """
+        To check if number is in allowed range [1-9999]
+    """
+    if n not in range(1,10000):
         error_msg = "Number not in range [1-9999]"
         raise Exception(error_msg)
 
-    while num != prev_num:
-        prev_num = num
-        str_num = str(num).zfill(4)
-        asc = int("".join(sorted(str_num)))
-        desc = int("".join(sorted(str_num, reverse=True)))
-        num = desc - asc
-        if num != prev_num:
-            steps.append(f"{desc} - {asc} = {num}")
+def get_steps(n):
+    """
+        Input  : 4 digit number
+        Output : Kaprekar routine steps to reach constant 0/6174.
+    """
+    _, steps = kaprekar_routine(n)
     return steps
 
-def step_count(num):
-    steps = get_steps(num)
-    return len(steps)
+def step_count(n):
+    """
+        Input  : 4 digit number
+        Output : No. of iterations to reach constant 0/6174.
+    """
+    count, _ = kaprekar_routine(n)
+    return count
