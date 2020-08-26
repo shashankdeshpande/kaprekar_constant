c9c78f051e109ee7263477987e98b5d48f32616f
shashank <shashank@lightinfosys.com>
2020-08-26 18:31:42 +0530

    initial commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name='kaprekar_constant',  
+    version='0.0.1',
+    scripts=['main.py'] ,
+    author="Shashank Deshpande",
+    author_email="shashankdeshpande18@gmail.com",
+    description="Kaprekar's Constant",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/shashankdeshpande/kaprekar-constant",
+    packages=find_packages()
+    )
