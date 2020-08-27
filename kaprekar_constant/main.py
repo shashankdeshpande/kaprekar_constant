0249df2528572974637139ee2da67a6ba4d03063
shashank <shashank@lightinfosys.com>
2020-08-27 13:22:30 +0530

    returning object

--- kaprekar_constant/main.py
+++ kaprekar_constant/main.py
@@ -1,14 +1,18 @@
-def kaprekar_routine(n):
+#!/usr/local/bin/python3
+
+from .object import Kaprekar
+
+def kaprekar_routine(n: int) -> object:
     """
         Input  : 4 digit Integer Number
                  1, 2, 3 digit numbers are also valid, we will pad those with leading zeros.
                  Allowed range - [1-9999]
 
-        Output : Kaprekar routine steps to reach constant 0/6174.
+        Output : Object containing constant, steps & step count
     """
     n = int(n)
     check_range(n)
-    routine = []
+    steps = []
     prev_n = 0
     while n != prev_n:
         prev_n = n
@@ -17,36 +21,24 @@ def kaprekar_routine(n):
         desc = int("".join(sorted(n_str, reverse=True)))
         n = desc - asc
         if n != prev_n:
-            routine.append(f"{desc} - {asc} = {n}")
-    return len(routine), routine
+            steps.append(f"{desc} - {asc} = {n}")
 
-def pad_leading_zeros(n):
+    constant = n
+    step_count = len(steps)
+    kc_obj = Kaprekar(constant, steps, step_count)
+    return kc_obj
+
+def pad_leading_zeros(n: int) -> str:
     """
         This function will pad 1,2,3 digit numbers with leading zeros and convert to string.
     """
     n_str = str(n).zfill(4)
     return n_str
 
-def check_range(n):
+def check_range(n: int) -> None:
     """
         To check if number is in allowed range [1-9999]
     """
     if n not in range(1,10000):
         error_msg = "Number not in range [1-9999]"
         raise Exception(error_msg)
-
-def get_steps(n):
-    """
-        Input  : 4 digit number
-        Output : Kaprekar routine steps to reach constant 0/6174.
-    """
-    _, steps = kaprekar_routine(n)
-    return steps
-
-def step_count(n):
-    """
-        Input  : 4 digit number
-        Output : No. of iterations to reach constant 0/6174.
-    """
-    count, _ = kaprekar_routine(n)
-    return count
